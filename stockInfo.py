74b1c80be2f64ad7b073f8aef5d69eaea224f113
moyuweiqing <49243973+moyuweiqing@users.noreply.github.com>
2020-11-11 09:33:14 +0800

    Update stockInfo.py

--- stockInfo.py
+++ stockInfo.py
@@ -1,54 +1,70 @@
 import pandas as pd
 import os
 import tushare as ts
-from pyecharts.charts import Line
+from pyecharts.charts import Kline
 import pyecharts.options as opts
 
 tsData = pd.DataFrame()
 date_list = []
-open_list = []
+price_list = []
 
 def draw():
-    line = (
-        Line(init_opts=opts.InitOpts(
-                                width='2000px',
+    kline = (
+        Kline(init_opts=opts.InitOpts(
+                                width='1800px',
                                 height='800px',
                                 page_title = "股票历史曲线图",
                                 js_host= "./",
                                 ))
             .set_global_opts(
-            tooltip_opts=opts.TooltipOpts(is_show=True),
-            xaxis_opts=opts.AxisOpts(type_="time"),
+            tooltip_opts=opts.TooltipOpts(
+                is_show = True,
+                axis_pointer_type = "line"),
+            xaxis_opts=opts.AxisOpts(
+                type_="time",
+                name = '日期',
+                split_number = 10),
             yaxis_opts=opts.AxisOpts(
                 type_="value",
+                name='价钱',
+                min_=12,
+                max_=18,
+                split_number = 5,
                 axistick_opts=opts.AxisTickOpts(is_show=True),
                 splitline_opts=opts.SplitLineOpts(is_show=True),
             ),
         )
-            .add_xaxis(xaxis_data=date_list)
+            # .add_xaxis(
+            # xaxis_data=date_list)
             .add_yaxis(
-            series_name="股市折线图",
-            y_axis=open_list,
-            symbol="circle",
-            is_symbol_show=True,
-            is_smooth = True,
-            label_opts=opts.LabelOpts(is_show=False),
+            series_name="股市K线图",
+            is_selected = True,
+            y_axis=price_list
         )
+
     )
-    line.render(path='first_bar.html')
+
+    kline.render(path='first_bar.html')
 
 def date_setting(start_date, end_date):
-    global tsData, date_list, open_list
+    global tsData, date_list, price_list
 
     tsData = ts.get_hist_data(code='000001', start=start_date, end=end_date)
     tsData = tsData.sort_index(ascending=True).reset_index()
 
     # 转成列表
     for i in range(0, len(tsData)):
+        alist = []
         date_list.append(tsData['date'].iloc[i])
-        open_list.append(tsData['open'].iloc[i])
-    print(date_list)
+        alist.append(tsData['date'].iloc[i])
+        alist.append(tsData['open'].iloc[i])
+        alist.append(tsData['close'].iloc[i])
+        alist.append(tsData['low'].iloc[i])
+        alist.append(tsData['high'].iloc[i])
 
+        price_list.append(alist)
+
+    # print(price_list)
 if __name__ == '__main__':
-    date_setting(start_date='2020-04-01', end_date='2020-10-01')
+    date_setting(start_date='2020-07-01', end_date='2020-10-01')
     draw()
